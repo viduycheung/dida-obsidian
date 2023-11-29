21b0f7470eebbabe5fcfc8df7c08a606813e240a
viduycheung <viduy@appest.com>
2023-11-29 09:25:32 +0800

    fix(request): fix no response request error heandle issue

--- main.ts
+++ main.ts
@@ -84,8 +84,12 @@ export default class TickTickPlugin extends Plugin {
 			name: "Fetch data",
 			callback: async () => {
 				if (this.checkUserLoginStatus()) {
-					await this.fetchUserData();
-					new Notice("Data fetched successfully");
+					try {
+						await this.fetchUserData();
+						new Notice("Data fetched successfully");
+					} catch (error) {
+						new Notice("Data fetched failed");
+					}
 				}
 			},
 		});
@@ -220,7 +224,7 @@ export default class TickTickPlugin extends Plugin {
 					break;
 				}
 				default:
-					break;
+					throw new Error(e);
 			}
 		});
 	};
@@ -245,7 +249,7 @@ export default class TickTickPlugin extends Plugin {
 					break;
 				}
 				default:
-					break;
+					throw new Error(e);
 			}
 		});
 	};
